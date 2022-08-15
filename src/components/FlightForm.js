4f5320892d0a94cc5386607324818be070d66c74
majhool <hacktonow@gmail.com>
2022-08-15 18:35:14 +0500

    flightcards

--- src/components/FlightForm.js
+++ src/components/FlightForm.js
@@ -164,6 +164,7 @@ function FlightForm() {
    console.log(flightsdata.origin)
   //this section for second form
   const [open, setOpen] = React.useState(false);
+  const [flightCards, setFlightCardsOpen] = React.useState(false);
 
   const handleClickOpen = (e) => {
     console.log('flyingTo, flyingFrom: ', flyingTo, flyingFrom);
@@ -180,7 +181,7 @@ function FlightForm() {
       }
       else{
        return(
-        console.log("else condtions")
+        setFlightCardsOpen(true)
         
        ) 
       }
@@ -191,6 +192,7 @@ function FlightForm() {
 
   const handleClose = () => {
     setOpen(false);
+    setFlightCardsOpen(false)
   };
   //this section for  email sending
   const form = useRef();
@@ -539,6 +541,41 @@ function FlightForm() {
           </div>}
 
 
+            {/* this section flightcards */}
+
+
+          {flightCards && <div className="flightCardsBackground">
+            <div className="flightCardsContainer">
+            <h3>Flights Information</h3>
+              <div className="flighttitleCloseBtn">
+                <button
+                  onClick={handleClose}
+                >
+                  X
+                </button>
+               
+              </div>
+              <DialogContent>
+               <div className='cardContainer'>
+                <div className='flightname'>
+                  <p>Lahore-Allama Iqbal intl.</p>
+                  <hr/>
+                  <p1>Karachi-Allama Iqbal intl.</p1>
+                 
+                </div>
+                <div className='timePrice'>
+                <p2>this section for time</p2>
+                <p3>this section for price</p3>
+                </div>
+
+               </div>
+
+            
+               </DialogContent>
+            </div>
+          </div>}
+
+
 
         </React.Fragment>
       </form>
@@ -576,144 +613,3 @@ function FlightForm() {
 }
 export default FlightForm;
 
-// airpot data
-const airports = [
-
-  { label: 'London, All Airports [LON], United Kingdom' },
-  { label: 'London, London Gatwick Arpt [LGW], United Kingdom' },
-  { label: 'London, London Heathrow Arpt [LHR], United Kingdom' },
-  { label: 'London, London Stansted Arpt [STN], United Kingdom' },
-  { label: 'London, Luton Arpt [LTN], United Kingdom' },
-  { label: 'London, London City Arpt [LCY], United Kingdom' },
-  { label: 'London, Ebbsfleet Intl [XQE], United Kingdom' },
-  { label: 'London, Ashford Intl [QDH], United Kingdom' },
-  { label: 'Islamabad, Islamabad Intl [ISB], Pakistan' },
-  { label: 'Karachi, Quaid E Azam International [KHI], Pakistan' },
-  { label: 'Lahore, Lahore Arpt [LHE], Pakistan' },
-  { label: 'Anaa Airport, Anaa, Tuamotus' },
-  { label: 'Arrabury Airport, Queensland, Australia' },
-  { label: 'El Arish International Airport, Egypt' },
-  { label: 'London, All Airports [LON], United Kingdom' },
-  { label: 'London, London Gatwick Arpt [LGW], United Kingdom' },
-  { label: 'London, London Heathrow Arpt [LHR], United Kingdom' },
-  { label: 'London, London Stansted Arpt [STN], United Kingdom' },
-  { label: 'London, Luton Arpt [LTN], United Kingdom' },
-  { label: 'London, London City Arpt [LCY], United Kingdom' },
-  { label: 'London, Ebbsfleet Intl [XQE], United Kingdom' },
-  { label: 'London, Ashford Intl [QDH], United Kingdom' },
-  { label: 'Manchester, Manchester Intl [MAN], United Kingdom' },
-  { label: 'Birmingham, Birmingham Intl Arpt [BHX], United Kingdom' },
-  { label: 'Edinburgh, Edinburgh Arpt [EDI], United Kingdom' },
-  { label: 'Glasgow, Glasgow Intl [GLA], United Kingdom' },
-  { label: 'Glasgow, Prestwick Arpt [PIK], United Kingdom' },
-  { label: 'Bristol, Bristol Intl Arpt [BRS], United Kingdom' },
-  { label: 'Newcastle, Newcastle Arpt [NCL], United Kingdom' },
-  { label: 'Liverpool, Liverpool Arpt [LPL], United Kingdom' },
-  { label: 'Leeds, Leeds Bradford Arpt [LBA], United Kingdom' },
-  { label: 'Nottingham Uk, East Midlands Arpt [EMA], United Kingdom' },
-  { label: 'Exeter, Exeter Arpt [EXT], United Kingdom' },
-  { label: 'Aberdeen, Aberdeen Airport [ABZ], United Kingdom' },
-  { label: 'Lagos, Murtala Muhammed Arpt [LOS], Nigeria' },
-  { label: 'Abuja, Abuja Intl [ABV], Nigeria' },
-  { label: 'Port Harcourt, Port Harcourt Arpt [PHC], Nigeria' },
-  { label: 'Kano, Aminu Kano Intl Arpt [KAN], Nigeria' },
-  { label: 'Enugu, Enugu [ENU], Nigeria' },
-  { label: 'Accra, Kotoka Airport [ACC], Ghana' },
-  { label: 'Nairobi, Jomo Kenyatta Intl [NBO], Kenya' },
-  { label: 'Mombasa, Moi Intl [MBA], Kenya' },
-  { label: 'Lamu, Lamu Arpt [LAU], Kenya' },
-  { label: 'Entebbe, Entebbe Airport [EBB], Uganda' },
-  { label: 'Toronto, Lester B Pearson Intl [YYZ], Canada' },
-  { label: 'Calgary, Calgary Intl Arpt [YYC], Canada' },
-  { label: 'Vancouver, Vancouver Intl Arpt [YVR], Canada' },
-  { label: 'Montreal, Pierre Elliott Trudeau Intl Arpt [YUL], Canada' },
-  { label: 'Ottawa, Ottawa Intl Arpt [YOW], Canada' },
-  { label: 'New York, John F Kennedy Intl [JFK], United States of America' },
-  { label: 'New York, Newark Intl Arpt [EWR], United States of America' },
-  { label: 'New York, All Airports [NYC], United States of America' },
-  { label: 'Seattle, Seattle Tacoma Intl Arpt [SEA], United States of America' },
-  { label: 'Miami, Miami Intl [MIA], United States of America' },
-  { label: 'Orlando, Orlando Intl Arpt [MCO], United States of America' },
-  { label: 'Chicago, A O Hare Intl [ORD], United States of America' },
-  { label: 'Los Angeles, Los Angeles Intl Arpt [LAX], United States of America' },
-  { label: 'Kingston, Norman Manly Arpt [KIN], Jamaica' },
-  { label: 'Montego Bay, Sangster Arpt [MBJ], Jamaica' },
-  { label: 'Port of Spain, Piarco Arpt [POS], Trinidad And Tobago' },
-  { label: 'Dhaka, Shahjalal International Airport [DAC], Bangladesh' },
-  { label: 'Ahmedabad, Ahmedabad Arpt [AMD], India' },
-  { label: 'Bengaluru, Bengaluru International airport [BLR], India' },
-  { label: 'Chennai, Chennai Arpt [MAA], India' },
-  { label: 'Mumbai, Chhatrapati Shivaji Intl [BOM], India' },
-  { label: 'Chandigarh, Chandigarh Arpt [IXC], India' },
-  { label: 'Delhi, Delhi Indira Gandhi Intl [DEL], India' },
-  { label: 'Kolkata , Netaji Subhas Chandra Bose Intl [CCU], India' },
-  { label: 'Hyderabad, Rajiv Gandhi Intl Arpt Shamshabad [HYD], India' },
-  { label: 'Kathmandu, Tribuvan Arpt [KTM], Nepal' },
-  { label: 'Colombo, Bandaranaike Intl Arpt [CMB], Sri Lanka' },
-  { label: 'Manila, Ninoy Aquino Intl [MNL], Philippines' },
-  { label: 'Cebu City, Cebu Intl [CEB], Philippines' },
-  { label: 'Angeles City, Clark International Arpt [CRK], Philippines' },
-  { label: 'Bangkok, Bangkok Intl Arpt [BKK], Thailand' },
-  { label: 'Bangkok, Don Mueang International [DMK], Thailand' },
-  { label: 'Phuket, Phuket Intl Airport [HKT], Thailand' },
-  { label: 'Beijing, Beijing Capital Arpt [PEK], China' },
-  { label: 'Guangzhou, Baiyun Airport [CAN], China' },
-  { label: 'Shanghai, Pu Dong Arpt [PVG], China' },
-  { label: 'Shenzhen, Shenzhen Arpt [SZX], China' },
-  { label: 'Sialkot, Sialkot Arpt [SKT], Pakistan' },
-  { label: 'Kabul, Khwaja Rawash Arpt [KBL], Afghanistan' },
-  { label: 'Moscow, Sheremetyevo Arpt [SVO], Russian Federation' },
-  { label: 'Moscow, Domodedovo Arpt [DME], Russian Federation' },
-  { label: 'Moscow, Vnukovo Arpt [VKO], Russian Federation' },
-  { label: 'St Petersburg, Pulkovo Airport [LED], Russian Federation' },
-  { label: 'Kiev, Borispol Arpt [KBP], Ukraine' },
-  { label: 'Kiev, Zhulhany Arpt [IEV], Ukraine' },
-  { label: 'Istanbul, Ataturk Arpt [IST], Turkey' },
-  { label: 'Istanbul, Sabiha Gokcen Arpt [SAW], Turkey' },
-  { label: 'Izmir, Adnan Menderes Airport [ADB], Turkey' },
-  { label: 'Dubai, Dubai Intl Arpt [DXB], United Arab Emirates' },
-  { label: 'Dubai, Al Maktoum Intl Arpt [DWC], United Arab Emirates' },
-  { label: 'Abu Dhabi, Dhabi Intl Arpt [AUH], United Arab Emirates' },
-  { label: 'Doha, Doha International Arpt [DOH], Qatar' },
-  { label: 'Kuwait, Kuwait Intl [KWI], Kuwait' },
-  { label: 'Muscat, Seeb Intl [MCT], Oman' },
-  { label: 'Bahrain, Bahrain Intl Arpt [BAH], Bahrain' },
-  { label: 'Riyadh, King Khaled Intl [RUH], Saudi Arabia' },
-  { label: 'Jeddah, Jeddah Intl [JED], Saudi Arabia' },
-  { label: 'Paris, Charles De Gaulle Intl Arpt [CDG], France' },
-  { label: 'Paris, Orly Arpt [ORY], France' },
-  { label: 'Nice, Cote D Azur Arpt [NCE], France' },
-  { label: 'Marseille, Marseille Provence Arpt [MRS], France' },
-  { label: 'Amsterdam, Schiphol Arpt [AMS], Netherlands' },
-  { label: 'Frankfurt, Frankfurt Intl [FRA], Germany' },
-  { label: 'Lisbon, Lisboa [LIS], Portugal' },
-  { label: 'Rome, Fiumicino Arpt [FCO], Italy' },
-  { label: 'Berlin, Berlin Brandenburg Airport [BER], Germany' },
-  { label: 'Adelaide, Adelaide Arpt [ADL], Australia' },
-  { label: 'Vienna, Vienna Intl Arpt [VIE], Austria' },
-  { label: 'Salzburg, W A Mozart Arpt [SZG], Austria' },
-  { label: 'Zurich, Zurich Airport [ZRH], Switzerland' },
-  { label: 'Geneva, Geneve Cointrin [GVA], Switzerland' },
-  { label: 'Warsaw, Warsaw Intl Arpt [WAW], Poland' },
-  { label: 'Freetown, Lungi Intl Arpt [FNA], Sierra Leone' },
-  { label: 'Banjul, Yundum Intl Arpt [BJL], The Gambia' },
-  { label: 'Mauritius, Plaisance Arptt [MRU], Mauritius' },
-  { label: 'Kuala Lumpur, Kuala Lumpur International Arpt [KUL], Malaysia' },
-  { label: 'Bandar Seri Begawan, Brunei Intl Arpt [BWN], Brunei' },
-  { label: 'Denpasar Bali, Ngurah Rai Arpt [DPS], Indonesia' },
-  { label: 'Cape Town, Cape Town International [CPT], South Africa' },
-  { label: 'Durban, Durban International [DUR], South Africa' },
-  { label: 'Johannesburg, Johannesburg International [JNB], South Africa' },
-  { label: 'Sydney, Sydney Kingsford Smith Arpt [SYD], Australia' },
-  { label: 'Melbourne, Tullamarine Arpt [MEL], Australia' },
-  { label: 'Brisbane, Brisbane Arpt [BNE], Australia' },
-  { label: 'Harare, Harare Arpt [HRE], Zimbabwe' },
-  { label: 'Kigali, Kayibanda Arpt [KGL], Rwanda' },
-  { label: 'Cairo, Cairo Intl Arpt [CAI], Egypt' },
-  { label: 'Casablanca, Mohamed V Arpt [CMN], Morocco' },
-  { label: 'Addis Ababa, Bole Airport [ADD], Ethiopia' },
-  { label: 'Brussels, Brussels National Arpt [BRU], Belgium' },
-
-
-
-];
